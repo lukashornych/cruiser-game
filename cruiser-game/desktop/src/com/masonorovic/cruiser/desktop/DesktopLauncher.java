a3c99c01ec950083e4259478a0dd96a56d199f8d
Masonorovic <l.masoncz@gmail.com>
2019-03-18 15:52:01 +0100

    Added base libgdx project for main game

---
+++ cruiser-game/desktop/src/com/masonorovic/cruiser/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.masonorovic.cruiser.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.masonorovic.cruiser.Cruiser;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		new LwjglApplication(new Cruiser(), config);
+	}
+}
